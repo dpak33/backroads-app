e9d82ec66f50b9a1bd5272941927f5397d274746
dpak33 <105285976+dpak33@users.noreply.github.com>
2023-05-25 17:36:38 +0100

    y

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React from 'react';
+import logo from '../images/logo.svg'
+import { pageLinks, socialLinks } from '../data';
 
 const Navbar = () => {
     return (
@@ -6,29 +8,33 @@ const Navbar = () => {
             <nav className="navbar">
                 <div className="nav-center">
                     <div className="nav-header">
-                        <img src="./images/logo.svg" className="nav-logo" alt="backroads" />
+                        <img src={logo} className="nav-logo" alt="backroads" />
                         <button type="button" className="nav-toggle" id="nav-toggle">
                             <i className="fas fa-bars"></i>
                         </button>
                     </div>
                     <ul className="nav-links" id="nav-links">
-                        <li>
-                            <a href="#home" className="nav-link"> home </a>
-                        </li>
+                        {pageLinks.map((link) => {
+                            return (
+                                <li key={link.id}>
+                                    <a href={link.href} className="nav-link">
+                                        {link.text}
+                                    </a>
+                                </li>
+                            );
+                        })}
 
-                        <li>
-                            <a href="#about" className="nav-link"> about </a>
-                        </li>
-
-                        <li>
-                            <a href="#services" className="nav-link"> services </a>
-                        </li>
-
-                        <li>
-                            <a href="#tours" className="nav-link"> tours</a>
-                        </li>
                     </ul>
-                    <ul className="nav-icons">
+                    <ul className="nav-icons" id="nav-icons">
+                        {socialLinks.map((link) => {
+                            const { href, icon, id } = link;
+                            return (
+                                <li key={id}>
+                                    <a href={href} icon={icon} className='nav-icon' />
+
+                                </li>
+                            )
+                        })}
                         <li>
                             <a href="https://www.twitter.com" target="_blank" className="nav-icon"
                             ><i className="fab fa-facebook"></i
