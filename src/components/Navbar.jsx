fc02986cf8088270dfafc41d36210f44c0ebe409
dpak33 <105285976+dpak33@users.noreply.github.com>
2023-05-26 14:04:36 +0100

    y

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -35,22 +35,8 @@ const Navbar = () => {
                                     </a>
                                 </li>
                             )
-                        })};
-                        <li>
-                            <a href="https://www.twitter.com" target="_blank" rel="noreferrer" className="nav-icon"
-                            ><i className="fab fa-facebook"></i
-                            ></a>
-                        </li>
-                        <li>
-                            <a href="https://www.twitter.com" target="_blank" rel="noreferrer" className="nav-icon"
-                            ><i className="fab fa-twitter"></i
-                            ></a>
-                        </li>
-                        <li>
-                            <a href="https://www.twitter.com" target="_blank" rel="noreferrer" className="nav-icon"
-                            ><i className="fab fa-squarespace"></i
-                            ></a>
-                        </li>
+                        })}
+
                     </ul>
                 </div>
             </nav>
